756be9b05897dbf4bacdde2cac6638b5a6f0d11e
RahulVavilala <132536039+RahulVavilala@users.noreply.github.com>
2024-12-08 18:31:14 -0500

    Initialize multi-participant

---
+++ public/js/script.js
@@ -0,0 +1,56 @@
+$(window).on("load", function () {
+  $(".ui.tiny.post.modal").modal({
+    observeChanges: true,
+  });
+
+  // Add new post Modal functionality
+  $("#newpost, a.item.newpost").click(function () {
+    $(".ui.tiny.post.modal").modal("show");
+  });
+
+  // new post validator (picture and text can not be empty); using Fomantic UI
+  $("#postform").form({
+    on: "blur",
+    fields: {
+      body: {
+        identifier: "body",
+        rules: [
+          {
+            type: "empty",
+            prompt: "Please add some text about your meal.",
+          },
+        ],
+      },
+      picinput: {
+        identifier: "picinput",
+        rules: [
+          {
+            type: "notExactly[/public/photo-camera.svg]",
+            prompt: "Please click on the Camera Icon to add a photo.",
+          },
+        ],
+      },
+    },
+    onSuccess: function (event, fields) {
+      $("#postform")[0].submit();
+      socket.emit("timeline activity", $("#session").attr("sessionID"));
+      $(".actions .ui.green.button").addClass("disabled");
+      $(".actions .ui.green.button").val("Posting...");
+    },
+  });
+
+  // Socket listening to broadcasts
+  // Incoming activity
+  socket.on("timeline activity", async function (sessionID) {
+    if (
+      sessionID === $("#session").attr("sessionID") &&
+      $("#newActivityMessage .ui.fixed.bottom.sticky").is(":hidden")
+    ) {
+      $("#newActivityMessage .ui.fixed.bottom.sticky").show();
+    }
+  });
+
+  $("#newActivityMessage .ui.fixed.bottom.sticky").on("click", function () {
+    location.reload();
+  });
+});
